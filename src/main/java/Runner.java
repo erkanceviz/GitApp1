81ea8e894c2f3eaaa5f547e9cb5fdb167164cd0b
ErkanCeviz <erkanceviz1985@gmail.com>
2024-05-24 21:23:41 +0200

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+    public static void main(String[] args) {
+
+// version 1 
+        String str="Hello Git";
+        System.out.println("Hello Git");
+
+        System.out.println("git add . : staging araya alir");
+
+
+    }
+}
